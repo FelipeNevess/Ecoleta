a5ece44f9d06260fa4bc007ead7e42dd7a244038
felipenevess <felipe012neves@gmail.com>
2022-01-09 16:03:44 -0300

    creating the items table

---
+++ service/src/database/migrations/01.create_items.ts
@@ -0,0 +1,13 @@
+import { Knex } from 'knex';
+
+exports.up = async (knex: Knex) => {
+  return knex.schema.createTable('items', table => {
+    table.increments('id').unsigned().primary();
+    table.string('image').notNullable();
+    table.string('title').notNullable();
+  });
+};
+
+exports.down = async (knex: Knex) => {
+  return knex.schema.dropTable('items');
+};

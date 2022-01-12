993ab8c66c28f91d4f9269467a1e5b45a65188d9
felipenevess <felipe012neves@gmail.com>
2022-01-11 21:51:13 -0300

    basic structure of the home page created

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import { Home } from './pages/Home/Home';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <p>fefeef</p>
+      <Home />
     </div>
   );
 }
